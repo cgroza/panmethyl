6d3f5290ef46cf0e7ed89767cc2839da82c907b7
Cristian Groza <cristian.groza@mail.mcgill.ca>
2024-09-10 12:10:44 -0400

    Enumerate signal at indexed CpGs

--- bin/nodes_methylation.py
+++ bin/nodes_methylation.py
@@ -1,14 +1,26 @@
-#!/usr/bin/env pypy3
-
 import pickle
 import sys
 
 nodes_list_path = sys.argv[1]
 pickle_mc_in = sys.argv[2]
-# pickle_cov_in = sys.argv[3]
-
 mc = pickle.load(open(pickle_mc_in, "rb"))
-# mcov = pickle.load(open(pickle_cov_in, "rb"))
+cpgs_index = gzip.open(sys.argv[3], "r")
+cpgs_index.readline()
+
+cpgs = dict()
+
+for line in cpgs_index:
+    node, pos, strand = line.decode().split()
+    pos = int(pos)
+    if not node in cpgs:
+      cpgs[node] = dict()
+
+
+    if not pos in cpgs[node]:
+        if strand == "+":
+            cpgs[node][pos] = 0
+        elif strand == "-":
+            cpgs[node][-pos] = 0
 
 nodes_list = set()
 with open(nodes_list_path, "r") as f:
@@ -16,11 +28,15 @@ with open(nodes_list_path, "r") as f:
         nodes_list.add(line.rstrip())
 
 for node in nodes_list:
-    # check if there is methylation signal on this node
-    if node in mc and len(mc[node]) > 0:
-        for pos in mc[node]:
+    # check if there is cpg on this node
+    if node in cpgs: #and len(mc[node]) > 0:
+        for pos in cpgs[node]:
             strand = "+"
             if pos < 0:
                 strand = "-"
-
-            print(node, abs(pos), strand, mc[node][pos][0], mc[node][pos][1]/mc[node][pos][0])
+            depth = 0
+            score = 0
+            if node in mc and pos in mc[node]:
+                depth = mc[node][pos][0]
+                score = mc[node][pos][1]/mc[node][pos][0]
+            print(node, abs(pos), strand, depth, score)
