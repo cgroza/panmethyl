ca8630ae12cbe09803f0f080d16455f77d0a610a
Cristian Groza <cristian.groza@mail.mcgill.ca>
2025-03-28 21:33:54 -0400

    Support multiple base modification types

--- tagtobed/src/main.rs
+++ tagtobed/src/main.rs
@@ -4,12 +4,14 @@ use std::io::Write;
 use std::env;
 use std::process;
 use std::iter::zip;
+use regex::Regex;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let mut opts = Options::new();
     opts.optopt("o", "", "set output file name", "NAME");
     opts.optopt("b", "", "set input bam", "NAME");
+    opts.optopt("B", "", "name of base modification", "NAME");
 
     let matches = match opts.parse(&args[1..]) {
         Ok(m) => { m }
@@ -20,9 +22,10 @@ fn main() {
     let mut out_file : Box<dyn Write> = if !matches.opt_present("o") {
         Box::new(std::io::stdout())
     } else {
-         Box::new(std::fs::File::create(matches.opt_str("o").unwrap()).unwrap())
+        Box::new(std::fs::File::create(matches.opt_str("o").unwrap()).unwrap())
     };
 
+    let base_mod = matches.opt_str("B").unwrap_or("C+m.".to_string());
 
     if !matches.opt_present("b") {
         eprintln!("No input provided.");
@@ -38,8 +41,6 @@ fn main() {
                 // retrieve tag and sequence
                 match record.tags().get(b"MM").or_else(| | record.tags().get(b"Mm") ) {
                     Some(bam::record::tags::TagValue::String(mm_tag_u8,  bam::record::tags::StringType::String))  => {
-                        match record.tags().get(b"ML").or_else(| | record.tags().get(b"Ml") ) {
-                            Some(bam::record::tags::TagValue::IntArray(ml_tag))  => {
                         // skip secondary and supplementary alignments
                         if record.flag().is_supplementary() || record.flag().is_secondary() {
                             continue
@@ -64,12 +65,14 @@ fn main() {
                         // parse mm tag and Cs in sequence
                         // assumes only one type of modification
                         let c_vec : Vec<usize> = seq.match_indices("C").map(|c| c.0).collect();
-                        let mm_indices : Vec<usize> = mm_tag.split(',').skip(1).
-                                    map(|i| { i.parse::<usize>().unwrap() }).collect();
 
+                        let re = Regex::new(r"^[0-9]+$").unwrap();
+                        let mm_pieces : Vec<&str> = mm_tag.split(|c: char| c == ',' || c == ';').collect();
 
-                        // let mm_probs : Vec<usize> = ml_tag.split(',').skip(1).
-                        //             map(|i| { i.parse::<usize>().unwrap() }).collect();
+                        // find the beginning of the desired base modification
+                        let mod_start = mm_pieces.iter().position(|&b| b.contains(&base_mod)).expect(format!("Could not find tag {}", base_mod).as_str());
+                        // take position until we meet the beginning of the next base modification
+                        let mm_indices : Vec<usize> = mm_pieces.iter().skip(mod_start + 1).take_while(|s| re.is_match(s)).map(|i| { i.parse::<usize>().unwrap() }).collect();
 
                         // calculate position of modified bases
                         let mut mod_bases : Vec<usize> = Vec::new();
@@ -87,12 +90,18 @@ fn main() {
                             i = i + 1;
                         }
 
-                        let _ = out_file.write_fmt(format_args!("{name}\t{bases}\n",
-                                 name = std::str::from_utf8(record.name()).unwrap(),
-                                 bases = zip(mod_bases, ml_tag.iter()).map(|b| b.0.to_string() + ":" + &b.1.to_string()).collect::<Vec<String>>().join(",")));
+                        let ml_tag : Vec<i32> = match record.tags().get(b"ML").or_else(| | record.tags().get(b"Ml") ) {
+                            Some(bam::record::tags::TagValue::IntArray(ml_tag))  => {
+                                // subset the ML values to the range specific to our base modification
+                                ml_tag.iter().skip(mod_start).take(mm_indices.len()).map(|e| e as i32).collect()
                             }
-                            _ => {} // for ml
-                        }
+                            _ => { std::iter::repeat(-1).take(mod_bases.len()).collect() } // for ml
+                        };
+
+                        let _ = out_file.write_fmt(format_args!("{name}\t{bases}\n",
+                                                                name = std::str::from_utf8(record.name()).unwrap(),
+                                                                bases = zip(mod_bases, ml_tag).map(|b| b.0.to_string() + ":" + &b.1.to_string()).collect::<Vec<String>>().join(",")));
+
                     }
                     _ => {}     // for mm
                 }
