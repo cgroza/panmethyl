ffd53032a33d065461d4ca55e4faa09807f31edd
Cristian Groza <cristian.groza@mail.mcgill.ca>
2025-05-23 20:06:44 -0400

    Run in constant memory.

--- bin/index_cpg.py
+++ bin/index_cpg.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env pypy3
-import pickle
 import sys
-
-cpg_index = dict()
 
 gfa = open(sys.argv[1])
 
@@ -14,25 +11,9 @@ for s in gfa:
         continue
     name = fields[1]
     seq = fields[2]
-#    if i % 1000 == 0:
-#      print("Processed " + str(i) + " nodes")
-    cpg_index[name] = set()
     cpg_i = seq.find("CG", 0)
     while cpg_i > -1:
-        cpg_index[name].add(cpg_i)
-        cpg_index[name].add(-(cpg_i + 1))
+        print(name, cpg_i, "+")
+        print(name, cpg_i + 1, "-")
         cpg_i = seq.find("CG", cpg_i + 2)
     i = i + 1
-
-# with open("gfa/cpg_index_xaf.pickle", "wb") as p:
-#     pickle.dump(cpg_index, p)
-
-
-# index = pickle.load(open("gfa/cpg_index_xaf.pickle", "rb"))
-
-for node in cpg_index:
-    for pos in cpg_index[node]:
-        strand = "+"
-        if pos < 0:
-            strand = "-"
-        print(node, abs(pos), strand)
