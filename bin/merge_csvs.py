9a78616ad20841161aaf1b9a3d9912a68aa494e8
Cristian Groza <cristian.groza@mail.mcgill.ca>
2025-10-15 20:10:00 -0400

    Merge CSVs with methylation data

---
+++ bin/merge_csvs.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import polars
+import sys
+
+polars.concat([polars.read_csv(f, separator=' ', has_header=False, new_columns=['node', 'pos', 'strand', 'gaf_depth', 'gaf_score']) for f in sys.argv[2:]], how = 'vertical') \
+      .group_by('node', 'pos', 'strand') \
+      .agg(score=polars.col("gaf_depth").dot("gaf_score") / polars.col("gaf_depth").sum(),
+           depth=polars.col("gaf_depth").sum()) \
+      .fill_nan(0) \
+      .sort('node', 'pos', 'strand') \
+      .write_csv(sys.argv[1], separator=' ')
+
