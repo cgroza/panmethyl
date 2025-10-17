c942dcbfd9e2829d73fd19d03a2ed4dcf7f2d002
Cristian Groza <cristian.groza@mail.mcgill.ca>
2025-10-17 15:30:48 -0400

    Explicit data types for polars

--- bin/merge_csvs.py
+++ bin/merge_csvs.py
@@ -2,7 +2,9 @@
 import polars
 import sys
 
-polars.concat([polars.read_csv(f, separator=' ', has_header=False, new_columns=['node', 'pos', 'strand', 'gaf_depth', 'gaf_score']) for f in sys.argv[2:]], how = 'vertical') \
+polars.concat([polars.read_csv(f, separator=' ', has_header=False,
+                               new_columns=['node', 'pos', 'strand', 'gaf_depth', 'gaf_score'],
+                               schema = [polars.String, polars.Int64, polars.String, polars.Float32, polars.Float32]) for f in sys.argv[2:]], how = 'vertical') \
       .group_by('node', 'pos', 'strand') \
       .agg(score=polars.col("gaf_depth").dot("gaf_score") / polars.col("gaf_depth").sum(),
            depth=polars.col("gaf_depth").sum()) \
