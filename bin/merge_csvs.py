d39e74044f28d2b0ecf7f66b98b1cbb623a5f816
Cristian Groza <cristian.groza@mail.mcgill.ca>
2025-10-20 12:01:51 -0400

    Dictionary schema

--- bin/merge_csvs.py
+++ bin/merge_csvs.py
@@ -4,7 +4,7 @@ import sys
 
 polars.concat([polars.read_csv(f, separator=' ', has_header=False,
                                new_columns=['node', 'pos', 'strand', 'gaf_depth', 'gaf_score'],
-                               schema = [polars.String, polars.Int64, polars.String, polars.Float32, polars.Float32]) for f in sys.argv[2:]], how = 'vertical') \
+                               schema = {'node':polars.String, 'pos':polars.Int64, 'strand':polars.String, 'gaf_depth':polars.Float32, 'gaf_score':polars.Float32}) for f in sys.argv[2:]], how = 'vertical') \
       .group_by('node', 'pos', 'strand') \
       .agg(score=polars.col("gaf_depth").dot("gaf_score") / polars.col("gaf_depth").sum(),
            depth=polars.col("gaf_depth").sum()) \
