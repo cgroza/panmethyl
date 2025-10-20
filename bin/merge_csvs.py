61b8181717a8c8ca1903d2d5a72afd508f43ea62
Cristian Groza <cristian.groza@mail.mcgill.ca>
2025-10-20 14:31:30 -0400

    Merge CSVs in constant memory

--- bin/merge_csvs.py
+++ bin/merge_csvs.py
@@ -1,14 +1,25 @@
 #!/usr/bin/python3
 import polars
 import sys
+import gzip
 
-polars.concat([polars.read_csv(f, separator=' ', has_header=False,
-                               new_columns=['node', 'pos', 'strand', 'gaf_depth', 'gaf_score'],
-                               schema = {'node':polars.String, 'pos':polars.Int64, 'strand':polars.String, 'gaf_depth':polars.Float32, 'gaf_score':polars.Float32}) for f in sys.argv[2:]], how = 'vertical') \
-      .group_by('node', 'pos', 'strand') \
-      .agg(score=polars.col("gaf_depth").dot("gaf_score") / polars.col("gaf_depth").sum(),
-           depth=polars.col("gaf_depth").sum()) \
-      .fill_nan(0) \
-      .sort('node', 'pos', 'strand') \
-      .write_csv(sys.argv[1], separator=' ')
+schema = {'node':polars.String, 'pos':polars.Int64, 'strand':polars.String, 'gaf_depth':polars.Float32, 'gaf_score':polars.Float32}
+columns = ['node', 'pos', 'strand', 'gaf_depth', 'gaf_score']
 
+chunk = polars.read_csv(sys.argv[2], separator=' ', has_header=False,
+                               new_columns=columns,
+                               schema = schema)
+
+if len(sys.argv) > 3:
+    for f in sys.argv[3:]:
+        chunk = polars.concat([chunk, polars.read_csv(f, separator=' ', has_header=False,
+                                                    new_columns=columns,
+                                                    schema=schema)],
+                              how = 'vertical') \
+                    .group_by('node', 'pos', 'strand') \
+                    .agg(score=polars.col("gaf_depth").dot("gaf_score")/polars.col("gaf_depth").sum(),
+                         depth=polars.col("gaf_depth").sum()) \
+                    .fill_nan(0)
+
+with gzip.open(sys.argv[1], 'wb') as csv:
+    chunk.sort('node', 'pos', 'strand').write_csv(csv, separator=' ')
