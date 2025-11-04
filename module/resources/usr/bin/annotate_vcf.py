660e2db1aaebb10611f943393897ba220f823352
Cristian Groza <cristian.groza@mail.mcgill.ca>
2025-11-04 14:11:08 -0500

    Add modification data to the INFO field

--- module/resources/usr/bin/annotate_vcf.py
+++ module/resources/usr/bin/annotate_vcf.py
@@ -30,6 +30,10 @@ for line in mods:
 mods.close()
 
 reader = vcfpy.Reader.from_path(vcf)
+reader.header.add_info_line({'ID' : 'NODE_POS', 'source' : 'panmethyl', 'Type' : 'String', 'Description' : 'Positions of modified nucleotides on the graph nodes.', 'Number' : 1})
+reader.header.add_info_line({'ID' : 'NODE_STRAND', 'source' : 'panmethyl', 'Type' : 'String', 'Description' : 'Strandness of modified nucletoides on the graph nodes.', 'Number' : 1})
+reader.header.add_info_line({'ID' : 'NODE_DEPTH', 'source' : 'panmethyl', 'Type' : 'String', 'Description' : 'Read depths on the modified nucleotides on the graph nodes.', 'Number' : 1})
+reader.header.add_info_line({'ID' : 'NODE_LEVEL', 'source' : 'panmethyl', 'Type' : 'String', 'Description' : 'Modification levels of nucleotides on the graph nodes, ranging from 0 to 255.', 'Number' : 1})
 writer = vcfpy.Writer.from_path(out_vcf, reader.header)
 
 for record in reader:
@@ -51,16 +55,16 @@ for record in reader:
         nodes_depths[g] = []
         nodes_levels[g] = []
         for node in paths[g]:
-            if node in node_mods:
-                nodes_positions[g].append(node + ":" + ",".join([n[0] for n in node_mods[node]]))
-                nodes_strands[g].append(node + ":" + ",".join([n[1] for n in node_mods[node]]))
-                nodes_depths[g].append(node + ":" + ",".join([n[2] for n in node_mods[node]]))
-                nodes_levels[g].append(node + ":" + ",".join([n[3] for n in node_mods[node]]))
+            if node[1] in node_mods:
+                nodes_positions[g].append(node[1] + ":" + ",".join([str(n[0]) for n in node_mods[node[1]]]))
+                nodes_strands[g].append(node[1] + ":" + ",".join([n[1] for n in node_mods[node[1]]]))
+                nodes_depths[g].append(node[1] + ":" + ",".join([n[2] for n in node_mods[node[1]]]))
+                nodes_levels[g].append(node[1] + ":" + ",".join([n[3] for n in node_mods[node[1]]]))
 
-    record.INFO["PNP"] = "|".join([g + "=" + ";".join(nodes_positions[g]) for g in nodes_positions])
-    record.INFO["PNS"] = "|".join([g + "=" + ";".join(nodes_strands[g]) for g in nodes_strands])
-    record.INFO["PND"] = "|".join([g + "=" + ";".join(nodes_depths[g]) for g in nodes_depths])
-    record.INFO["PNL"] = "|".join([g + "=" + ";".join(nodes_levels[g]) for g in nodes_levels])
+    record.INFO["NODE_POS"] = ["|".join([str(g) + "=" + ";".join(nodes_positions[g]) for g in nodes_positions])]
+    record.INFO["NODE_STRAND"] = ["|".join([str(g) + "=" + ";".join(nodes_strands[g]) for g in nodes_strands])]
+    record.INFO["NODE_DEPTH"] = ["|".join([str(g) + "=" + ";".join(nodes_depths[g]) for g in nodes_depths])]
+    record.INFO["NODE_LEVEL"] = ["|".join([str(g) + "=" + ";".join(nodes_levels[g]) for g in nodes_levels])]
 
     writer.write_record(record)
 
