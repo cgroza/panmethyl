2174f2aff9dbf06bc61204b0f1bbf235f8415536
Cristian Groza <cristian.groza@mail.mcgill.ca>
2025-12-05 15:13:50 -0500

    Output TSV but also annotate VCF

--- module/resources/usr/bin/annotate_vcf.py
+++ module/resources/usr/bin/annotate_vcf.py
@@ -20,20 +20,22 @@ out_vcf = sys.argv[3]
 
 node_mods = {}
 
+print("CHROM", "POS", "ID", "allele", "path", "node_positions",
+      "strands", "depths", "levels", sep='\t')
+
 for line in mods:
     node, pos, strand, depth, level = line.decode().split()
-    pos = int(pos)
     if node not in node_mods:
         node_mods[node] = []
-    node_mods[node].append((pos, strand, depth, level))
+    node_mods[node].append((int(pos), strand, float(depth), float(level)))
 
 mods.close()
 
 reader = vcfpy.Reader.from_path(vcf)
-reader.header.add_info_line({'ID' : 'NODE_POS', 'source' : 'panmethyl', 'Type' : 'String', 'Description' : 'Positions of modified nucleotides on the graph nodes.', 'Number' : 1})
-reader.header.add_info_line({'ID' : 'NODE_STRAND', 'source' : 'panmethyl', 'Type' : 'String', 'Description' : 'Strandness of modified nucletoides on the graph nodes.', 'Number' : 1})
-reader.header.add_info_line({'ID' : 'NODE_DEPTH', 'source' : 'panmethyl', 'Type' : 'String', 'Description' : 'Read depths on the modified nucleotides on the graph nodes.', 'Number' : 1})
-reader.header.add_info_line({'ID' : 'NODE_LEVEL', 'source' : 'panmethyl', 'Type' : 'String', 'Description' : 'Modification levels of nucleotides on the graph nodes, ranging from 0 to 255.', 'Number' : 1})
+reader.header.add_format_line({'ID' : 'PMN', 'source' : 'panmethyl', 'Type' : 'Float', 'Description' : 'Number of modified nucleotides on the path.', 'Number' : '.'})
+reader.header.add_format_line({'ID' : 'PML', 'source' : 'panmethyl', 'Type' : 'Float', 'Description' : 'Average modification levels across the the path.', 'Number' : '.'})
+reader.header.add_format_line({'ID' : 'PMD', 'source' : 'panmethyl', 'Type' : 'Float', 'Description' : 'Average depth across the modified nucleotides.', 'Number' : '.'})
+
 writer = vcfpy.Writer.from_path(out_vcf, reader.header)
 
 for record in reader:
@@ -44,28 +46,40 @@ for record in reader:
         if g is not None:
             paths[g] = parse_path_re(record.INFO['AT'][g])
 
-    nodes_positions = {}
-    nodes_strands = {}
-    nodes_depths = {}
-    nodes_levels = {}
-
+    pmn = {}
+    pml = {}
+    pmd = {}
     for g in paths:
-        nodes_positions[g] = []
-        nodes_strands[g] = []
-        nodes_depths[g] = []
-        nodes_levels[g] = []
+        nodes_positions = []
+        nodes_strands = []
+        nodes_depths = []
+        nodes_levels = []
+
+        pmn[g] = 0
+        pml[g] = 0
+        pmd[g] = 0
         for node in paths[g]:
             if node[1] in node_mods:
-                nodes_positions[g].append(node[1] + ":" + ",".join([str(n[0]) for n in node_mods[node[1]]]))
-                nodes_strands[g].append(node[1] + ":" + ",".join([n[1] for n in node_mods[node[1]]]))
-                nodes_depths[g].append(node[1] + ":" + ",".join([n[2] for n in node_mods[node[1]]]))
-                nodes_levels[g].append(node[1] + ":" + ",".join([n[3] for n in node_mods[node[1]]]))
+                nodes_positions.append(node[1] + ":" + ",".join([str(n[0]) for n in node_mods[node[1]]]))
+                nodes_strands.append(",".join([n[1] for n in node_mods[node[1]]]))
+                nodes_depths.append(",".join([str(n[2]) for n in node_mods[node[1]]]))
+                nodes_levels.append(",".join([str(n[3]) for n in node_mods[node[1]]]))
+        print(record.CHROM, record.POS, record.ID[0], str(g),
+              record.INFO['AT'][g],
+              ";".join(nodes_positions),
+              ",".join(nodes_strands),
+              ",".join(nodes_depths),
+              ",".join(nodes_levels),
+              sep='\t')
 
-    record.INFO["NODE_POS"] = ["|".join([str(g) + "=" + ";".join(nodes_positions[g]) for g in nodes_positions])]
-    record.INFO["NODE_STRAND"] = ["|".join([str(g) + "=" + ";".join(nodes_strands[g]) for g in nodes_strands])]
-    record.INFO["NODE_DEPTH"] = ["|".join([str(g) + "=" + ";".join(nodes_depths[g]) for g in nodes_depths])]
-    record.INFO["NODE_LEVEL"] = ["|".join([str(g) + "=" + ";".join(nodes_levels[g]) for g in nodes_levels])]
+        pmn[g] = len([float(d) for node in nodes_depths for d in node.split(',')])
+        if pmn[g] != 0:
+            pml[g] = sum([float(l) for node in nodes_levels for l in node.split(',')])/pmn[g]
+            pmd[g] = sum([float(d) for node in nodes_depths for d in node.split(',')])/pmn[g]
 
+    record.add_format("PMN", [str(pmn[g]) for g in pmn])
+    record.add_format("PML", [str(pml[g]) for g in pml])
+    record.add_format("PMD", [str(pmd[g]) for g in pmd])
     writer.write_record(record)
 
 writer.close()
