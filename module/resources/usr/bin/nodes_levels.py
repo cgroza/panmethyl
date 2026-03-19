d79215cdc9e0677403d29ba14eef4fdec94ec6c3
Cristian Groza <cristian.groza@mail.mcgill.ca>
2026-03-19 12:35:30 -0400

    fix(nodes_levels): print original position value instead of absolute value

--- module/resources/usr/bin/nodes_levels.py
+++ module/resources/usr/bin/nodes_levels.py
@@ -32,4 +32,4 @@ for line in cpgs_index:
     if node in mc and pos in mc[node]:
         depth = mc[node][pos][0]
         score = mc[node][pos][1]
-    print(node, abs(pos), strand, depth, score, pair, sep='\t')
+    print(node, pos, strand, depth, score, pair, sep='\t')
